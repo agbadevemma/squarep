6d888b2f39102743b2596fa90fc7c4c9f02ebf4e
Emmanuel <agbadevemmanuel@gmail.com>
2025-03-26 23:01:08 +0100

    created a banner section

---
+++ src/sharedcomponents/BannerSection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type BannerSectionProps = {
+  bg: { src: string };
+  title: string;
+  description: string;
+};
+const BannerSection = ({ bg, title, description }: BannerSectionProps) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${bg.src})` }}
+      className="w-full h-64 bg-cover bg-center"
+    >
+      <div className="w-full h-64 bg-black/60 text-white flex flex-col items-center justify-center">
+        <span className="text-4xl font-semibold">{title}</span>
+        <p className="text-sm mt-2 text-center max-w-4xl">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BannerSection;
